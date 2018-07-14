d2ccffdb5fdabfaad18a9e84c02d7b2bc147f7d9
Korbinian Kasberger <korbi87@gmail.com>
2018-07-14 16:13:42 +0200

    Finished bottle javascript

--- src/bottle.js
+++ src/bottle.js
@@ -17,7 +17,9 @@ import {loadSingleProductionCard} from "./processing.js"
 
 // field.getHarvestPointer!
 
-
+/**
+ * Function that loads the cards for the final bottle
+ */
 export async function getBottleDetails(production) {
     $("#detailsModal").modal("hide");
     $("#bottle").removeClass("d-none");
@@ -36,6 +38,11 @@ export async function getBottleDetails(production) {
 }
 
 
+/**
+ * Adds the production card to the UI
+ *
+ * @param {*} production Address of the production
+ */
 async function loadProductionCard(production){
     empty("productions");
     $('#productions-col').find(".loader").removeClass("d-none");
@@ -44,6 +51,11 @@ async function loadProductionCard(production){
 
 } 
 
+/**
+ * Function to load all transports as cards
+ *
+ * @param {*} transports The tranpsort addresses
+ */
 async function loadTransportCards(transports){
     empty("transports");
     $('#transports-col').find(".loader").removeClass("d-none");
@@ -57,12 +69,12 @@ async function loadTransportCards(transports){
     $('#transports-col').find(".loader").addClass("d-none");
 }
 
-async function loadHarvestCards(harvests){
-    for (let i = 0; i < harvests.length; i++) {
-        document.getElementById("bottle-harvests").innerHTML +=  await loadSingleHarvest(harvests[i]);
-    }
-}
 
+/**
+ * Loads all fields as cards
+ *
+ * @param {*} fields Addresses of the fields
+ */
 async function loadFieldCards(fields){
     empty("fields");
     $('#fields-col').find(".loader").removeClass("d-none");
@@ -72,53 +84,84 @@ async function loadFieldCards(fields){
     $('#fields-col').find(".loader").addClass("d-none");
 }
 
+/**
+ * Retrieves a transport included in a production
+ *
+ * @param {*} production Address of the production
+ * @returns The address of the transport
+ */
 async function getTransportFromProduction(production) {
     $('#productions-col').find(".loader").removeClass("d-none");
     const processhandler_instance = await processHandler_contract(web3.currentProvider).deployed();
-    const transport =  await processhandler_instance.getTransportFromProduction.call(production);
+    const transport =  await processhandler_instance.getTransportFromProduction.call(production).catch(err => console.error(err));
     return transport;
 }
 
+/**
+ * Empties a bottle column by an id
+ *
+ * @param {*} id The ID of the column
+ */
 function empty(id){
     $("#bottle-"+id).empty();
 }
 
+/**
+ * Retrieves all Transports from a production
+ *
+ * @param {*} production Address of a production
+ * @returns Addresses of the transports
+ */
 async function getTransportsFromProduction(production) {
     const processhandler_instance = await processHandler_contract(web3.currentProvider).deployed();
-    const transports =  await processhandler_instance.getTransportsFromProduction.call(production);
+    const transports =  await processhandler_instance.getTransportsFromProduction.call(production).catch(err => console.error(err));
     return transports;
 }
 
-
+/**
+ * Retrieves all field addresses included in a harvest
+ *
+ * @param {*} harvest Address of the harvest
+ * @returns Addresses of the fields
+ */
 async function getFieldsFromHarvest(harvest) {
     const harvest_instance = await harvest_contract(web3.currentProvider).at(harvest);
-    const fields = await harvest_instance.getFields.call().then((result) => {
-        return result;
-    });
+    const fields = await harvest_instance.getFields.call().catch(err => console.error(err));
     return fields;
 }
 
+/**
+ * Retrieves the harvest address from a transport
+ *
+ * @param {*} transport The address of the transport
+ * @returns Address of the Harvest
+ */
 async function getHarvestFromTransport(transport) {
     const transportHandler_instance = await transportHandler_contract(web3.currentProvider).deployed();
-    const harvest = await transportHandler_instance.getHarvestFromTransport.call(transport).then((result) => {
-        return result;
-    });
+    const harvest = await transportHandler_instance.getHarvestFromTransport.call(transport).catch(err => console.error(err));
     return harvest;
 }
 
+/**
+ * Retrieves all harvest addresses included in a transport
+ *
+ * @param {*} transport Address of the transport
+ * @returns Addresses of the harvests
+ */
 async function getHarvestsFromTransport(transport) {
     const transportHandler_instance = await transportHandler_contract(web3.currentProvider).deployed();
-    const harvest = await transportHandler_instance.getHarvestsFromTransport.call(transport).then((result) => {
-        return result;
-    });
+    const harvest = await transportHandler_instance.getHarvestsFromTransport.call(transport).catch(err => console.error(err));
     return harvest;
 }
 
 
+/**
+ * Gets the Address of the current production
+ *
+ * @returns Address of the production
+ */
 async function currentProduction() {
     const processHandler_instance = await processHandler_contract(web3.currentProvider).deployed();
-    const production = await processHandler_instance.currentProduction.call().then((production) => {
-        return production;
-    });
+    const production = await processHandler_instance.currentProduction.call().catch(err => console.error(err));
     return production;
 }
